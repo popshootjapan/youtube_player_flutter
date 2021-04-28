a39aa0d59bc941f724992c3708fc631d116822e6
omatty <9mm.cool@gmail.com>
2021-04-28 20:16:45 +0900

    Fix crash under iOS 14.5, in order not to add 'null'

--- packages/youtube_player_flutter/lib/src/player/raw_youtube_player.dart
+++ packages/youtube_player_flutter/lib/src/player/raw_youtube_player.dart
@@ -381,5 +381,5 @@ class _RawYoutubePlayerState extends State<RawYoutubePlayer>
 
   String get userAgent => controller.flags.forceHD
       ? 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
-      : null;
+      : '';
 }
